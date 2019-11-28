56c84ba6eeddfee819646307c7bf9f4cacf4a22c
Virendra Singh <virendra.bisht2006@gmail.com>
2019-11-28 18:53:53 +0000

    This is my first commit

---
+++ src/main/java/com/mock/preparation/basic/FirstProgram.java
@@ -0,0 +1,11 @@
+package com.mock.preparation.basic;
+
+/*
+first program to print hello
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
+
