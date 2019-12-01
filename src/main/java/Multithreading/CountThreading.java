298c30f71c1a8d1e5663ba69590a577a160e1ff7
Virendra Singh <virendra.bisht2006@gmail.com>
2019-12-01 14:05:08 +0000

    Count Thread

---
+++ src/main/java/Multithreading/CountThreading.java
@@ -0,0 +1,29 @@
+package Multithreading;
+
+public class CountThreading extends Thread {
+    CountThreading() {
+        super(" Count the Threading ");
+        System.out.println("Process of counting started" + this);
+        start();
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i <= 10; i++)
+                System.out.println("Thread started to run " + i);
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Threading started");
+        }
+    }
+}
+
+class threadextendsthread {
+    public static void main(String[] args) {
+        CountThreading ct = new CountThreading();
+        while (ct.isAlive()) ;
+        System.out.println("Threading counter finished ");
+
+
+    }
+}
