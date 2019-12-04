9cf3bdabf7f6bbb9df2fa879aa04610022da849e
Virendra Singh <virendra.bisht2006@gmail.com>
2019-12-04 19:12:53 +0000

    Array Pattern

---
+++ src/main/java/Ducat/ArrayPattern7.java
@@ -0,0 +1,21 @@
+package Ducat;
+
+public class ArrayPattern7 {
+    public static void main(String[] args) {
+        /*
+             A B C D E
+             A B C D E
+             A B C D E
+             A B C D E
+             A B C D E
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print((char) (j + 65) + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
