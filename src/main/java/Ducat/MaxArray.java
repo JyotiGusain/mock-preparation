c07e19d633851f971364f59c3787abb01e22ce77
Virendra Singh <virendra.bisht2006@gmail.com>
2019-12-04 17:48:57 +0000

    Maximum Number in Array

---
+++ src/main/java/Ducat/MaxArray.java
@@ -0,0 +1,24 @@
+package Ducat;
+
+/*
+To find the maximum number in the array
+
+Enter the size of an array: [I@6e0be858
+The max number in the array is: 78
+ */
+public class MaxArray {
+    public static void main(String[] args) {
+        int z[] = new int[]{3, 45, 78, 12, 34, 4};
+
+        int maximum = 0;
+
+        System.out.println("Enter the size of an array: " + z);
+
+        for (int i = 1; i < z.length; i++) {
+            if (z[i] > maximum)
+                maximum = z[i];
+        }
+        System.out.println("The max number in the array is: " + maximum);
+
+    }
+}
