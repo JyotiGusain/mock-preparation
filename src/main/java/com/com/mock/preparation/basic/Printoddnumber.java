43a359db2da8a6de4afc6bd4fcf8a679bb344f04
Virendra Singh <virendra.bisht2006@gmail.com>
2019-12-14 13:51:54 +0000

    Odd Numbers

---
+++ src/main/java/com/com/mock/preparation/basic/Printoddnumber.java
@@ -0,0 +1,13 @@
+package com.mock.preparation.basic;
+
+public class Printoddnumber {
+    public static void main(String[] args) {
+        {
+            for (int i = 1; i <= 100; i++) {
+                if (i % 2 != 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
