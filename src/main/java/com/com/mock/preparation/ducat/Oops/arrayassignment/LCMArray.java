2594b2cc02de11f5fec9009c50aeb3a0d10a8588
Virendra Singh <virendra.bisht2006@gmail.com>
2019-12-20 15:59:40 +0000

    Find the LCM

--- src/main/java/com/com/mock/preparation/ducat/Oops/arrayassignment/LCMArray.java
+++ src/main/java/com/com/mock/preparation/ducat/Oops/arrayassignment/LCMArray.java
@@ -3,20 +3,28 @@ package com.mock.preparation.ducat.Oops.arrayassignment;
 import java.util.Scanner;
 
 public class LCMArray {
+    /*
+    Find the LCM
+
+    Please Enter the First Integer Value of Num1 : 3
+
+     Please Enter the Second Integer Value of Num2 : 4
+
+     GCD of 3 and 4  =  1
+
+     LCM of 3 and 4  =  12
+
+     */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         int Num1, Num2, HCF = 0, LCM = 0;
+        Scanner sc = new Scanner(System.in);
         sc = new Scanner(System.in);
-
-        System.out.print(" Please Enter the First Integer Value : ");
+        System.out.print(" Please Enter the  Integer Value of Num1 & Num2 : ");
         Num1 = sc.nextInt();
-
-        System.out.print(" Please Enter the Second Integer Value : ");
         Num2 = sc.nextInt();
-
         int a = Num1;
         int b = Num2;
-
         while (Num2 != 0) {
             if (Num1 > Num2) {
                 Num1 = Num1 - Num2;
