de96a4aba5d2d728746468d53c4ec3ac31fe11b4
Virendra Singh <virendra.bisht2006@gmail.com>
2019-12-20 16:00:05 +0000

    Find the HCF

--- src/main/java/com/com/mock/preparation/ducat/Oops/arrayassignment/HCFArray.java
+++ src/main/java/com/com/mock/preparation/ducat/Oops/arrayassignment/HCFArray.java
@@ -3,12 +3,22 @@ package com.mock.preparation.ducat.Oops.arrayassignment;
 import java.util.Scanner;
 
 public class HCFArray {
+    /*
+    Find the HCF
+
+    enter the interger value of a:
+    60
+    enter the interger value of b:
+    72
+    HCF of 60 and 72 is 12
+
+     */
     public static void main(String[] args) {
         int a, b, hcf = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the interger value: ");
+        System.out.println("enter the interger value of a: ");
         a = sc.nextInt();
-        System.out.println("enter the interger value: ");
+        System.out.println("enter the interger value of b: ");
         b = sc.nextInt();
 
         for (int i = 1; i <= a && i <= b; i++) {
