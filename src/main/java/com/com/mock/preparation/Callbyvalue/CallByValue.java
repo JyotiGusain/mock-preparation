cc52b41ecdba9431b4e12ae6bcf284ccd1fedb84
Virendra Singh <virendra.bisht2006@gmail.com>
2020-05-01 05:38:46 +0100

    Call by Value

--- src/main/java/com/com/mock/preparation/Callbyvalue/CallByValue.java
+++ src/main/java/com/com/mock/preparation/Callbyvalue/CallByValue.java
@@ -1,5 +1,4 @@
 package com.mock.preparation.Callbyvalue;
-
 /*
 In case of Call By Value the original value will not change
 Before Change: 100
@@ -11,11 +10,11 @@ public class CallByValue {
     public static void main(String[] args) {
         CallByValue callByValue = new CallByValue();
         System.out.println("Before Change: " + callByValue.data);
-        callByValue.change(500);
+        callByValue.change(callByValue);
         System.out.println("After Change: " + callByValue.data);
     }
 
-    void change(int data) {
+    void change(CallByValue callByValue) {
         data = data + 10;
     }
 }
